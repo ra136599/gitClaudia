670c8e7a9d2f4545349a016732cdff95e8d07fa7
ra136599 <162502128+ra136599@users.noreply.github.com>
2024-04-01 19:12:50 -0300

    Update pares.js

--- pares.js
+++ pares.js
@@ -1,5 +1,5 @@
-const array = [2,4,3,1,6,7,9];
+const array = [2,4,3,1,6,7,9,10,13,15,22];
 
 const pares = array.filter((item) => item % 2 === 0);
 
-console.log("Números pares: ",pares);
+console.log("Os Números PARES são: ",pares);
